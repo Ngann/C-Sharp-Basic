8d6bf86dcd17883d3dcf89b6d746115cc19ae1b9
Ngan Nguyen <ngancamnguyen@gmail.com>
2019-08-13 19:18:00 -0700

    add student object to main

--- Program.cs
+++ Program.cs
@@ -15,31 +15,39 @@ namespace myApp
         {   
             //SchoolTracker for each item display grade
 
-            // var student = new Student();
-            // student.Name = "Jim";
-            // Console.Write(student.Name);
+            var students = new List<Student>();
 
-            var studentNames = new List<string>();
-            var studentGrades = new List<int>();
             var adding = true;
 
             while(adding)
             {
-                Console.Write("Student Name: ");
-                studentNames.Add(Console.ReadLine());
+                var newStudent = new Student();
 
-                Console.Write("Student Grade: ");
-                studentGrades.Add(int.Parse(Console.ReadLine()));
+                Console.Write("Student Name:");
+                newStudent.Name = Console.ReadLine();
+
+                Console.Write("Student Grade:");
+                newStudent.Grade = int.Parse(Console.ReadLine());
+
+                Console.Write("Student Birthday:");
+                newStudent.Birthday = Console.ReadLine();
+
+                Console.Write("Student Address:");
+                newStudent.Address = Console.ReadLine();
+
+                Console.Write("Student Phone:");
+                newStudent.Phone = int.Parse(Console.ReadLine());
+
+                students.Add(newStudent);
 
                 Console.WriteLine("Add another? y/n");
 
                 if(Console.ReadLine() != "y");
                     adding = false;
             }
-
-            for (int i = 0; i < studentNames.Count; i++)
+            foreach (var student in students)
             {
-                Console.WriteLine("Name: {0}, Grade:{1}", studentNames[i], studentGrades[i]);
+                Console.WriteLine("Name: {0}, Grade:{1}", student.Name, student.Grade);
             }
             
         }
